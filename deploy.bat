be9252bf64fc5781856cd7c1648176e078662e5a
Brad Vincent <bradvin@gmail.com>
2017-04-11 13:52:45 +0200

    added deploy file

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+xcopy dist\core D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\shared /S /Y /Q
+
+xcopy dist\templates\image-viewer\css\foogallery.image-viewer.min.css D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\image-viewer\css\foogallery.image-viewer.min.css /Y /Q
+xcopy dist\templates\image-viewer\js\foogallery.image-viewer.min.js D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\image-viewer\js\foogallery.image-viewer.min.js /Y /Q
+
+xcopy dist\templates\justified\css\foogallery.justified.min.css D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\justified\css\foogallery.justified.min.css /Y /Q
+xcopy dist\templates\justified\js\foogallery.justified.min.js D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\justified\js\foogallery.justified.min.js /Y /Q
+
+xcopy dist\templates\masonry\css\foogallery.masonry.min.css D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\masonry\css\foogallery.masonry.min.css /Y /Q
+xcopy dist\templates\masonry\js\foogallery.masonry.min.js D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\masonry\js\foogallery.masonry.min.js /Y /Q
+
+xcopy dist\templates\responsive\css\foogallery.responsive.min.css D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\default\css\foogallery.responsive.min.css /Y /Q
+xcopy dist\templates\responsive\js\foogallery.responsive.min.js D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\default\js\foogallery.responsive.min.js /Y /Q
+
+xcopy dist\templates\simple-portfolio\css\foogallery.simple-portfolio.min.css D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\simple-portfolio\css\foogallery.simple-portfolio.min.css /Y /Q
+xcopy dist\templates\simple-portfolio\js\foogallery.simple-portfolio.min.js D:\Websites\wp-latest-foo\wp-content\plugins\foogallery\extensions\default-templates\simple-portfolio\js\foogallery.simple-portfolio.min.js /Y /Q
