4a867b85d948debf9fef720fcaa92c5a91a9f8bc
Steve Usher <steveush@gmail.com>
2018-03-08 23:43:22 +0200

    Updated selectify method to handle empty strings and objects.

--- src/core/js/_foogallery.js
+++ src/core/js/_foogallery.js
@@ -1,4 +1,4 @@
-(function($, _, _utils, _is, _fn){
+(function ($, _, _utils, _is, _fn) {
 
 	_.debug = new _utils.Debugger("__FooGallery__");
 
@@ -9,20 +9,21 @@
 	 * @param {(string|string[]|object)} classes - A single space delimited string of CSS class names to convert or an array of them with each item being included in the selector using the OR (`,`) syntax as a separator. If an object is supplied the result will be an object with the same property names but the values converted to selectors.
 	 * @returns {(object|string)}
 	 */
-	_utils.selectify = function(classes){
-		if (_is.hash(classes)){
+	_utils.selectify = function (classes) {
+		if (_is.empty(classes)) return null;
+		if (_is.hash(classes)) {
 			var result = {}, selector;
-			for (var name in classes){
+			for (var name in classes) {
 				if (!classes.hasOwnProperty(name)) continue;
-				if (selector = _utils.selectify(classes[name])){
+				if (selector = _utils.selectify(classes[name])) {
 					result[name] = selector;
 				}
 			}
 			return result;
 		}
-		if (_is.string(classes) || _is.array(classes)){
+		if (_is.string(classes) || _is.array(classes)) {
 			if (_is.string(classes)) classes = [classes];
-			return $.map(classes, function(str){
+			return $.map(classes, function (str) {
 				return _is.string(str) ? "." + str.split(/\s/g).join(".") : null;
 			}).join(",");
 		}
@@ -56,17 +57,17 @@
 	 */
 	_.dataItem = "__FooGalleryItem__";
 
-	_.init = function(options, element){
+	_.init = function (options, element) {
 		return _.template.make(options, element).initialize();
 	};
 
-	_.initAll = function(options){
-		return _fn.when($(".foogallery").map(function(i, element){
+	_.initAll = function (options) {
+		return _fn.when($(".foogallery").map(function (i, element) {
 			return _.init(options, element);
 		}).get());
 	};
 
-	_.parseSrc = function(src, srcWidth, srcHeight, srcset, renderWidth, renderHeight){
+	_.parseSrc = function (src, srcWidth, srcHeight, srcset, renderWidth, renderHeight) {
 		if (!_is.string(src)) return null;
 		// if there is no srcset just return the src
 		if (!_is.string(srcset)) return src;
@@ -94,8 +95,8 @@
 
 		// get the current viewport info and use it to determine the correct src to load
 		var dpr = window.devicePixelRatio || 1,
-			area = {w: renderWidth * dpr, h: renderHeight * dpr, x: dpr},
-			property;
+				area = {w: renderWidth * dpr, h: renderHeight * dpr, x: dpr},
+				property;
 
 		// first check each of the viewport properties against the max values of the same properties in our src array
 		// only src's with a property greater than the viewport or equal to the max are kept
@@ -142,15 +143,15 @@
 	 *     <div class="fg-item-inner">
 	 *       <a class="fg-thumb" href="[item.href]">
 	 *         <img class="fg-image" width="[item.width]" height="[item.height]"
-	 *         	title="[item.title]" alt="[item.description]"
-	 *         	data-src="[item.src]"
-	 *         	data-srcset="[item.srcset]" />
+	 *          title="[item.title]" alt="[item.description]"
+	 *          data-src="[item.src]"
+	 *          data-srcset="[item.srcset]" />
 	 *         <!-- Optional caption markup -->
 	 *         <div class="fg-caption">
-	 *         	<div class="fg-caption-inner">
-	 *         	 <div class="fg-caption-title">[item.title]</div>
-	 *         	 <div class="fg-caption-desc">[item.description]</div>
-	 *         	</div>
+	 *          <div class="fg-caption-inner">
+	 *           <div class="fg-caption-title">[item.title]</div>
+	 *           <div class="fg-caption-desc">[item.description]</div>
+	 *          </div>
 	 *         </div>
 	 *       </a>
 	 *     </div>
@@ -158,17 +159,17 @@
 	 *   <!-- Any number of additional items -->
 	 * </div>
 	 * <script>
-	 * 	jQuery(function($){
+	 *  jQuery(function($){
 	 * 		$("#gallery-1").foogallery();
 	 * 	});
 	 * </script>
 	 * @example {@caption Options can be supplied directly to the `.foogallery()` method or by supplying them using the `data-foogallery` attribute. If supplied using the attribute the value must follow [valid JSON syntax](http://en.wikipedia.org/wiki/JSON#Data_types.2C_syntax_and_example) including quoted property names. If the same option is supplied in both locations as it is below, the value from the attribute overrides the value supplied to the method, in this case `lazy` would be `true`.}{@lang html}
 	 * <!-- Supplying the options using the attribute -->
 	 * <div id="gallery-1" class="foogallery fg-responsive" data-foogallery='{"lazy": true}'>
-	 * 	<!-- Items -->
+	 *  <!-- Items -->
 	 * </div>
 	 * <script>
-	 * 	jQuery(function($){
+	 *  jQuery(function($){
 	 * 		// Supply the options directly to the method
 	 * 		$("#gallery-1").foogallery({
 	 * 			lazy: false
@@ -176,12 +177,12 @@
 	 * 	});
 	 * </script>
 	 */
-	$.fn.foogallery = function(options, ready){
-		return this.filter(".foogallery").each(function(i, element){
-			if (_is.string(options)){
+	$.fn.foogallery = function (options, ready) {
+		return this.each(function (i, element) {
+			if (_is.string(options)) {
 				var template = $.data(element, _.dataTemplate);
-				if (template instanceof _.Template){
-					switch (options){
+				if (template instanceof _.Template) {
+					switch (options) {
 						case "layout":
 							template.layout();
 							return;
@@ -191,8 +192,8 @@
 					}
 				}
 			} else {
-				_.template.make(options, element).initialize().then(function(template){
-					if (_is.fn(ready)){
+				_.template.make(options, element).initialize().then(function (template) {
+					if (_is.fn(ready)) {
 						ready(template);
 					}
 				});
@@ -213,9 +214,9 @@
 	 */
 
 })(
-	FooGallery.$,
-	FooGallery,
-	FooGallery.utils,
-	FooGallery.utils.is,
-	FooGallery.utils.fn
+		FooGallery.$,
+		FooGallery,
+		FooGallery.utils,
+		FooGallery.utils.is,
+		FooGallery.utils.fn
 );
