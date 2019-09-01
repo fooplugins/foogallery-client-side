1f6e0067095e180e2b2f57062154bb79fe607999
Steve Usher <steveush@gmail.com>
2019-09-01 10:28:02 +0200

    Added new base EventComponent.

--- src/core/js/Component.js
+++ src/core/js/Component.js
@@ -29,6 +29,36 @@
 		}
 	});
 
+	_.EventComponent = _utils.EventClass.extend(/** @lend FooGallery.EventComponent */{
+		/**
+		 * @summary The base class for all child components of a {@link FooGallery.Template|template} that raise there own events.
+		 * @constructs
+		 * @param {FooGallery.Template} template - The template creating the component.
+		 * @augments FooGallery.utils.EventClass
+		 * @borrows FooGallery.utils.Class.extend as extend
+		 * @borrows FooGallery.utils.Class.override as override
+		 */
+		construct: function(template){
+			this._super(template);
+			/**
+			 * @summary The template that created this component.
+			 * @memberof FooGallery.EventComponent#
+			 * @name tmpl
+			 * @type {FooGallery.Template}
+			 */
+			this.tmpl = template;
+		},
+		/**
+		 * @summary Destroy the component making it ready for garbage collection.
+		 * @memberof FooGallery.EventComponent#
+		 * @function destroy
+		 */
+		destroy: function(){
+			this._super();
+			this.tmpl = null;
+		}
+	});
+
 	/**
 	 * @summary A factory for registering and creating basic gallery components.
 	 * @memberof FooGallery
