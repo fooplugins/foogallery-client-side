bed1f51ce3b60df3aa5eac5da82c1d156046037f
Steve Usher <steveush@gmail.com>
2020-05-04 19:29:23 +0200

    Updated the grunt build to fix issue with missing polyfills in free. Removed `dist/full_polyfill` folder as both versions now need the polyfills and updated the deploy files.

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 xcopy dist\lite D:\Websites\foo.dev.cc\wp-content\plugins\foogallery\extensions\default-templates\shared /S /Y /Q
-xcopy dist\full_polyfill D:\Websites\foo.dev.cc\wp-content\plugins\foogallery\pro\extensions\default-templates\shared /S /Y /Q
+xcopy dist\full D:\Websites\foo.dev.cc\wp-content\plugins\foogallery\pro\extensions\default-templates\shared /S /Y /Q
 
 xcopy dist\admin\css\foogallery.admin.min.css D:\Websites\foo.dev.cc\wp-content\plugins\foogallery\css\foogallery.admin.min.css /Y /Q
 xcopy dist\admin\js\foogallery.admin.min.js D:\Websites\foo.dev.cc\wp-content\plugins\foogallery\js\foogallery.admin.min.js /Y /Q
