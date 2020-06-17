c6b78d5c1308c06e3838e18668ba35c4ff979c19
Steve Usher <steveush@gmail.com>
2020-06-17 17:55:12 +0200

    Updated dist and version 1.4.15

--- dist/admin/js/foogallery.admin.js
+++ dist/admin/js/foogallery.admin.js
@@ -1,11 +1,21 @@
 (function($){
 
 	$(function () {
-		$(".foogallery-vertical-tab").on("click", function(e){
+		$(".foogallery-vertical-tab,.foogallery-vertical-child-tab").on("click", function(e){
 			e.preventDefault();
-			var $this = $(this), $settings = $this.closest(".foogallery-settings"), name = $this.data("name");
+			e.stopPropagation();
+			var $this = $(this),
+				name = $this.data("name"),
+				selector = '[data-name="'+name+'"]',
+				$settings = $this.closest(".foogallery-settings"),
+				$content = $settings.find('.foogallery-tab-contents').find(selector),
+				$parent = $this.closest(".foogallery-vertical-tab"),
+				$child = $parent.find(selector);
+
+			// first remove old active classes
 			$settings.find(".foogallery-tab-active").removeClass("foogallery-tab-active");
-			$settings.find('[data-name="'+name+'"]').addClass("foogallery-tab-active");
+			// now re-apply the active classes to the elements that need it
+			$parent.add($child).add($content).addClass("foogallery-tab-active");
 		});
 	});
 
