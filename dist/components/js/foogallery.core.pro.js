a70f0c1add77b69d98e7d2d10a0069eeceff74aa
steveush <steveush@gmail.com>
2021-04-11 11:10:51 +0200

    Version 2.0.9

--- dist/components/js/foogallery.core.pro.js
+++ dist/components/js/foogallery.core.pro.js
@@ -5863,7 +5863,7 @@ FooGallery.utils.$, FooGallery.utils, FooGallery.utils.is, FooGallery.utils.fn);
 						}
 						tmpl.items.create(available, true);
 					}
-					if (obj.item){
+					if (obj.item instanceof _.Item){
 						if (self.opt.scrollTo) {
 							obj.item.scrollTo();
 						}
