e5d60736e0416bab312f1ef2c0882b0c93705b53
steveush <steveush@gmail.com>
2022-03-22 20:01:27 +0200

    Contains basic multi-level filtering options for testing.

---
+++ test-pages/templates/assets/multi-level-filtering.js
@@ -0,0 +1,14 @@
+FooGallery.auto({
+    "filtering": {
+        "tags": [{
+            "all": "All",
+            "tags": ["tag1","tag2"]
+        },{
+            "all": "All",
+            "tags": ["tag3","tag4"]
+        },{
+            "all": "All",
+            "tags": ["tag5","unique"]
+        }]
+    }
+});
