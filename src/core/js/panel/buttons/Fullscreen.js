a37b79fe42fcbc1317406e641b93856ebdc9f177
Steve Usher <steveush@gmail.com>
2023-09-27 14:01:49 +0200

    Fix to hide fullcreen button on devices that do not support it.

--- src/core/js/panel/buttons/Fullscreen.js
+++ src/core/js/panel/buttons/Fullscreen.js
@@ -11,6 +11,9 @@
                 toggle: true
             });
         },
+        isEnabled: function(){
+            return this._super() && _.fullscreen.supported;
+        },
         click: function(){
             var self = this, pnl = self.panel.el;
             _.fullscreen.toggle(pnl).then(function(){
