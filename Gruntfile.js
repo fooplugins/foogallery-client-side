5e747e9b9b1afb5e6d0edaf0b87771a3bbc9e1e8
Steve Usher <steveush@gmail.com>
2026-03-07 16:47:27 +0200

    Initial checkin of dominant color.

--- Gruntfile.js
+++ Gruntfile.js
@@ -179,7 +179,9 @@ module.exports = function ( grunt ) {
 						"./src/core/css/hover-effects/transitions/scale.css",
 						"./src/core/css/hover-effects/transitions/zoomed.css",
                         "./src/core/css/hover-effects/transitions/semi-zoomed.css",
-						"./src/core/css/hover-effects/transitions/slide-up-right-down-left.css"
+						"./src/core/css/hover-effects/transitions/slide-up-right-down-left.css",
+
+						"./src/core/css/appearance/dominant-color.css"
 					]
 				}
 			},
